b207e98b8a639bd5536020f43a0a4b6eb7873cfc
Ana <1179design@mail.ru>
2023-10-07 19:00:12 +0700

    added code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Самостоятельная работа
+// Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+
+
+
+      {
+        Console.WriteLine("Введите длины трех сторон треугольника:");
+
+        // Ввод длин сторон
+        Console.Write("Сторона A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Сторона B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Сторона C: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Проверка условия треугольника
+        if (IsTriangle(a, b, c))
+        {
+            Console.WriteLine("Треугольник существует.");
+        }
+        else
+        {
+            Console.WriteLine("Треугольник не существует.");
+        }
+    }
+
+    // Функция проверки неравенства треугольника
+    static bool IsTriangle(double a, double b, double c)
+    {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
